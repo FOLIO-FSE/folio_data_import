117dcee44403d95a8894c28af9bfb4db622c1d54
Brooks Travis <brooks.travis@gmail.com>
2025-10-31 00:43:09 -0500

    Add function to retrieve FOLIO connection parameters interactively, and set version

---
+++ src/folio_data_import/__init__.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+import questionary
+
+def get_folio_connection_parameters(gateway_url, tenant_id, username, password):
+    if not gateway_url:
+        gateway_url = questionary.text("Enter FOLIO Gateway URL:").ask()
+    if not tenant_id:
+        tenant_id = questionary.text("Enter FOLIO Tenant ID:").ask()
+    if not username:
+        username = questionary.text("Enter FOLIO Username:").ask()
+    if not password:
+        password = questionary.password("Enter FOLIO password:").ask()
+    return gateway_url, tenant_id, username, password
+
+__version__ = importlib.metadata.version("folio-data-import")
